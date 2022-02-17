fa377329f9713f53b4ad2fb8539e5018b022148d
Ece <74778171+kedimonmuo@users.noreply.github.com>
2022-02-17 16:20:41 +0100

    second commit : play function needs to be fixed

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-window.addEventListener("load",  () => {
-    const sounds = document.querySelectorAll ("beat");
-    const pads = document.querySelectorAll(".pads div");
+window.addEventListener("load",  function () {
+        const audios = document.querySelectorAll("beat");
+        const pads = document.querySelectorAll(".pads div");
 
-// how to make audios function
-    pads.forEach(pad, index) =>  {
-        pad.addEventListener ("click", function() {
-            sounds[index].play();
+        // how to make audios function
+        pads.forEach ((pad, index) => {
+            pad.addEventListener("click", function () {
+                audios[index].play();
+            });
+
         });
-        
     });
-});
+
+console.log("audio");
